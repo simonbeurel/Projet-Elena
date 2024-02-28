a975eb9ce575ce0fd59357d05c4a8705e04894e7
simonbeurel <simon.beurel@etu.unice.fr>
2024-02-28 16:44:10 +0000

    Add a first step for scrapping every player's last 10 matches

--- util.py
+++ util.py
@@ -1,6 +1,18 @@
 '''
 The goal of this file is to define some useful functions for our DB
 '''
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver_path = '/usr/local/bin/chromedriver'
+
+
 
 def parsing_db():
     #Open and read the original file
@@ -27,5 +39,36 @@ def retrieve_player_id(first_name, last_name):
             nom_de_famille = element.split("-")[0]
         if element.split("-")[-2]==first_name and nom_de_famille==last_name:
             return element.split("-")[-1]
+
+
+def retrieve_player_statsAce(player_name, player_id):
+    # player_name = rybakina-elena
+    # player_id = UDzElXdm
+
+    driver = webdriver.Firefox()
+
+    url = f"https://www.flashscore.fr/joueur/{player_name}/{player_id}/"
+    driver.get(url)
+
+    elem = driver.find_elements(By.CLASS_NAME, "event__match")
+    print(f"Nombre de matchs trouvés : {len(elem)}")
+
+    id_matches = []
+    for element in elem:
+        id_matches.append(element.get_attribute("id")[4:])
+
+    for id in id_matches:
+        if id==id_matches[0]:
+            print("on passe")
+        else:
+            driver.get(f"https://www.flashscore.fr/match/{id}/#/resume-du-match/statistiques-du-match/0")
+            sleep(2)
+            element_data_test_id = driver.find_elements(By.TAG_NAME, 'strong')
+            print(f"Nombre d'éléments : {len(element_data_test_id)}")
+            for strong_element in element_data_test_id:
+                if strong_element.text == "Aces":
+                    print(strong_element)
+
+    driver.close()
 
 
