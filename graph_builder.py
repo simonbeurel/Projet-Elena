69484aa5957acb9cd0a28ecc51274509e7fb2601
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2024-03-27 20:55:23 +0100

    Add some new functions for graph_builder

--- graph_builder.py
+++ graph_builder.py
@@ -3,7 +3,7 @@ The main goal of this file is to create graph
 '''
 import matplotlib.pyplot as plt
 import numpy as np
-from util import retrieve_player_statsAce, retrieve_player_ranking_receiver_ladder, retrieve_player_id
+from util import retrieve_player_statsAce, retrieve_player_ranking_receiver_ladder, retrieve_player_id, retrieve_player_ranking_server_ladder
 
 
 def build_graph_aces_last_10_matches(array_aces, playername):
@@ -50,9 +50,67 @@ def build_graph_aces_against_ranked_receiver(array_aces, array_opponents, player
 
     plt.show()
 
+
+def build_graph_receive_stats_last_10_matches(array_receive, playername):
+    moyenne = sum(array_receive) / len(array_receive)
+    plt.plot(range(len(array_receive)), array_receive, marker='o', linestyle='-')
+    plt.axhline(y=moyenne, color='r', linestyle='--', label=f'Moyenne: {moyenne}')
+    plt.xlabel('Index')
+    plt.ylabel('Valeur')
+    plt.title(f"Nombre de d'aces pris sur les 10 derniers matchs de {playername}")
+    plt.legend()
+    plt.show()
+
+
+def build_graph_aces_against_ranked_server(array_receive, array_opponents, playername, nameOpponent):
+    print(array_opponents)
+    array_rankings_opponents = []
+    for i in range(len(array_opponents)):
+        result = retrieve_player_ranking_server_ladder(array_opponents[i])
+        if result==None:
+            print(f"Le joueur {array_opponents[i]} n'a pas de ranking")
+        array_rankings_opponents.append(result)
+
+    #remove every None in the array
+    while None in array_rankings_opponents:
+        array_rankings_opponents.remove(None)
+
+    array_rankings_opponents.append('0')
+    array_receive.append(0)
+
+    array_rankings_opponents = [int(rank) for rank in array_rankings_opponents]
+
+    #trier le tableau des rankings
+    array_rankings_opponents, array_receive = zip(*sorted(zip(array_rankings_opponents, array_receive)))
+
+    ranking_opponent_target = retrieve_player_ranking_server_ladder(nameOpponent)
+    plt.plot(array_rankings_opponents, array_receive, marker='o', linestyle='-')
+    plt.title(f"Nombre d'aces de {playername} en fonction du rank de l'adversaire")
+    plt.xlabel("Rank de l'adversaire")
+    plt.ylabel("Nombre d'aces")
+    aces_value = np.interp(ranking_opponent_target, array_rankings_opponents, array_receive)
+
+    print()
+    print("Ranking de l'adversaire: ", ranking_opponent_target)
+    print("Nombre d'aces attendus: ", aces_value)
+
+    plt.show()
+
+
+
 playerName="garcia-caroline"
 id_player = "8fw1CCQC"
 result = retrieve_player_statsAce(playerName,id_player)
-#print(result)
+'''PREDICTION AVERAGE ACES MIS'''
 #build_graph_aces_last_10_matches(result[0][::-1], playerName)
-build_graph_aces_against_ranked_receiver(result[0][::-1], result[2][::-1], playerName, "Danielle Collins")
+'''PREDICTION 1'''
+#build_graph_aces_against_ranked_receiver(result[0][::-1], result[2][::-1], playerName, "Danielle Collins")
+
+
+playerName="collins-danielle"
+id_player = "pS5UjYbG"
+result = retrieve_player_statsAce(playerName,id_player)
+'''PREDICTION AVERAGE ACES PRIS'''
+#build_graph_receive_stats_last_10_matches(result[1][::-1], playerName)
+'''PREDICTION 2'''
+build_graph_aces_against_ranked_server(result[1][::-1], result[2][::-1], playerName, "Caroline Garcia")
